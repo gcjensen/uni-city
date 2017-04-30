30e4d5bf5122e69db6f58f14cd8c4e23e51a42b4
George <gj4g13@soton.ac.uk>
2017-04-30 07:58:12 +0100

    Configured swagger to provide api documentation (localhost:8080/docs)

--- server/server.js
+++ server/server.js
@@ -1,13 +1,34 @@
 var express    = require('express');
 var app        = express();
+var subpath    = express();
 var path       = require('path');
 var bodyParser = require('body-parser');
+var argv = require('minimist')(process.argv.slice(2));
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/node_modules', express.static(__dirname + '../node_modules'));
 app.use('../server/resources', express.static(__dirname + '../server/resources'));
+
+/*
+ * For creating Swagger documentation
+ *
+ * ------------------------------------------------
+ */
+app.use(bodyParser());
+var swagger = require('swagger-node-express').createNew(subpath);
+app.use(express.static('server/dist'));
+
+app.get('/docs', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
+});
+
+// Set api-doc path
+swagger.configureSwaggerPaths('', 'server/dist/api-docs', '');
+swagger.configure('http://localhost:8080/', '1.0.0');
+
+/* ------------------------------------------------ */
 
 app.use('/api', require('./app/controllers/cities'));
 app.use('/api', require('./app/controllers/rent'));
